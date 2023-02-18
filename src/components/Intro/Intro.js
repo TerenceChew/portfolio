103196c16949eacc59084247792e3ece0f57d436
TerenceChew <wschew97@gmail.com>
2023-02-18 04:55:56 -0500

    Add animations to Intro, update components style
    - Add typing & blinking animations to Intro
    - Update z-index of FullPageNav & BurgerMenu
    - Update CSS variables

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,7 +4,10 @@ const Intro = () => {
   return (
     <section id="intro" className="intro section">
       <div className="greeting">Hi there! My name is</div>
-      <div className="name">Terence.</div>
+      <div className="name-box flex">
+        <span className="prompt">{">"}</span>
+        <span className="name">Terence.</span>
+      </div>
       <h1>Front-End Web Developer.</h1>
       <h1>I build things and make them pretty on the Internet.</h1>
       <button className="btn">Resume</button>
