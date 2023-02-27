ee1b51f910476ae40fd38f14951c6b5cf026e4f9
TerenceChew <wschew97@gmail.com>
2023-02-27 06:58:30 -0500

    Update AboutMe content

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,28 +5,17 @@ const AboutMe = () => {
     <section id="about-me" className="about-me section">
       <h2>About Me</h2>
       <p>
-        {"Hello! My name is "}
-        <span>Terence</span>
-        {" and I am a self-taught "}
-        <span>JavaScript</span>
-        {" developer. I enjoy learning and building new things."}
+        A quick learner, responsible and motivated person who enjoys problem
+        solving and building pretty things on the internet.
       </p>
       <p>
-        {"Ever since I started dabbling in HTML and CSS, I knew that "}
-        <code>this</code>
-        {
-          " is what I want to do in my career. From then on, I've been exploring more about Front-End technologies like "
-        }
-        <span>React</span>
-        {" and use them to build exciting projects."}
+        My daily routine consists of learning new skills as well as technologies
+        and implementing them to build exciting projects.
       </p>
       <p>
-        {"Currently, I'm planning to dive deeper into the "}
-        <span>Back-End</span>
-        {" side of web development, particularly technologies such as "}
-        <span>PostgreSQL</span>
-        {" and "}
-        <span>Node.js</span>.
+        Currently in search of new opportunities, through which I aim to grow
+        individually as well as a developer, with an ultimate goal of becoming a
+        full-stack developer.
       </p>
     </section>
   );
