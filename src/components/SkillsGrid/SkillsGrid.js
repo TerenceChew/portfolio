3b953d2bb7a35f06b0699dda1f5dbb6ef5bb7214
TerenceChew <wschew97@gmail.com>
2023-07-04 08:08:37 -0400

    Update SkillsGrid

--- src/components/SkillsGrid/SkillsGrid.js
+++ src/components/SkillsGrid/SkillsGrid.js
@@ -8,10 +8,22 @@ import git from "./assets/svgs/git.svg";
 import npm from "./assets/svgs/npm.svg";
 import psql from "./assets/svgs/psql.svg";
 import webpack from "./assets/svgs/webpack.svg";
+import jest from "./assets/svgs/jest.svg";
+import nodejs from "./assets/svgs/nodejs.svg";
+import knexjs from "./assets/svgs/knexjs.svg";
+import express from "./assets/svgs/express.svg";
 import SkillBox from "../SkillBox/SkillBox";
 
 const Skills = () => {
   const skills = [
+    {
+      name: "HTML",
+      img: html,
+    },
+    {
+      name: "CSS",
+      img: css,
+    },
     {
       name: "JavaScript",
       img: js,
@@ -33,16 +45,24 @@ const Skills = () => {
       img: webpack,
     },
     {
+      name: "Jest",
+      img: jest,
+    },
+    {
       name: "PostgreSQL",
       img: psql,
     },
     {
-      name: "HTML",
-      img: html,
+      name: "Node.js",
+      img: nodejs,
     },
     {
-      name: "CSS",
-      img: css,
+      name: "Express",
+      img: express,
+    },
+    {
+      name: "Knex.js",
+      img: knexjs,
     },
   ];
   const skillBoxes = skills.map((skill) => (
