e203c1cbdd8fe80c817e8cd6a477534b163b3da7
TerenceChew <wschew97@gmail.com>
2023-07-27 13:21:24 -0400

    Fix body unscrollable if resize closes FullPageNav
    - Body was unable to scroll if resize closes FullPageNav, because shouldShowFullPageNav is still true
    - onresize will now check if width >= 950, if true, shouldShowFullPageNav is set to false, allowing body to scroll
    
    Remarks:
    - Moved debounce & handleResize from App to Header, to avoid passing unnecessary props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,20 @@ import Nav from "../Nav/Nav";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import FullPageNav from "../FullPageNav/FullPageNav";
 
-const Header = ({ windowWidth }) => {
+const debounce = (cb, delay = 500) => {
+  let timeout;
+
+  return (...args) => {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
+
+const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [shouldShowFullPageNav, setShouldShowFullPageNav] = useState(false);
 
   const toggleFullPageNav = () => {
@@ -22,6 +35,23 @@ const Header = ({ windowWidth }) => {
       document.body.style.overflowY = "scroll";
     }
   }, [shouldShowFullPageNav]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+
+      if (window.innerWidth >= 950) {
+        setShouldShowFullPageNav(false);
+      }
+    };
+    const debouncedHandleResize = debounce(handleResize);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  }, []);
 
   return (
     <div id="header" className="header flex f-center">
--- src/container/App.js
+++ src/container/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "../components/Header/Header";
 import Intro from "../components/Intro/Intro";
@@ -9,20 +9,7 @@ import Contacts from "../components/Contacts/Contacts";
 import Resume from "../components/Resume/Resume";
 import Footer from "../components/Footer/Footer";
 
-const debounce = (cb, delay = 500) => {
-  let timeout;
-
-  return (...args) => {
-    clearTimeout(timeout);
-
-    timeout = setTimeout(() => {
-      cb(...args);
-    }, delay);
-  };
-};
-
 const App = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [shouldShowResume, setShouldShowResume] = useState(false);
 
   const toggleResume = () => {
@@ -33,22 +20,9 @@ const App = () => {
     setShouldShowResume(true);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    const debouncedHandleResize = debounce(handleResize);
-
-    window.addEventListener("resize", debouncedHandleResize);
-
-    return () => {
-      window.removeEventListener("resize", debouncedHandleResize);
-    };
-  });
-
   return (
     <div className="app">
-      <Header windowWidth={windowWidth} />
+      <Header />
       <div>
         <Intro showResume={showResume} />
         <AboutMe />
