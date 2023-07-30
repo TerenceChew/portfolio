76a7e2c338411803ee4e70af7eca14e0c73b319c
TerenceChew <wschew97@gmail.com>
2023-07-30 04:19:37 -0400

    Change Header position to fixed
    - This is done to simplify user interactions

--- src/components/ScrollToTopIcon/ScrollToTopIcon.js
+++ src/components/ScrollToTopIcon/ScrollToTopIcon.js
@@ -2,7 +2,7 @@ import "./ScrollToTopIcon.css";
 
 const ScrollToTopIcon = () => {
   return (
-    <a href="#header">
+    <a href="#intro">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="70"
